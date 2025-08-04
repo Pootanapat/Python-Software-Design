b37bdf758a560726edee216b15b3f3dfa10a6494
teetachkakanamporn <68030125@kmitl.ac.th>
2025-08-04 20:56:18 +0700

    เพิ่มงานของซันคุง777

--- Real/sunkung/bk.py
+++ Real/sunkung/bk.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+import  firebase_admin
+from firebase_admin import credentials, firestore
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate('path/to/your/serviceAccountKey.json')
+firebase_admin.initialize_app(cred)             
+# Initialize Firestore
+db = firestore.client()     
+# Function to add a document to a collection
+def add_document(collection_name, document_data):           
+    collection_ref = db.collection(collection_name)
+    collection_ref.add(document_data)
+    
