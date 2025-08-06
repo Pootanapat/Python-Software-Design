1c89d205b432db2c39123732d14d4528b968606e
teetachkakanamporn <68030125@kmitl.ac.th>
2025-08-06 08:50:05 +0700

    firebase

--- Real/sunkung/bk.py
+++ Real/sunkung/bk.py
@@ -1,12 +1,7 @@
 import  firebase_admin
 from firebase_admin import credentials, firestore
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate('path/to/your/serviceAccountKey.json')
-firebase_admin.initialize_app(cred)             
-# Initialize Firestore
-db = firestore.client()     
-# Function to add a document to a collection
-def add_document(collection_name, document_data):           
-    collection_ref = db.collection(collection_name)
-    collection_ref.add(document_data)
+#เชืิ่อมต่อ Firebase
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
     
