25a840ae062e54b9683963c8e7b65a8fe068a7a7
teetachkakanamporn <teetachkakanamporn@gmail.com>
2025-08-07 22:34:09 +0700

    1

--- Real/sunkung/bk.py
+++ Real/sunkung/bk.py
@@ -4,4 +4,8 @@ from firebase_admin import credentials, firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-#ฟังก์ชันสำหรับเพิ่มข้อมูล
+
+class FramManager:
+    def __init__(self):
+        self.db = db
+
