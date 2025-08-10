2201a54529ac00e8c594fa7e94c5cbb008baf89a
teetachkakanamporn <teetachkakanamporn@gmail.com>
2025-08-10 11:33:07 +0700

    Update bk.py

--- Real/sunkung/bk.py
+++ Real/sunkung/bk.py
@@ -5,9 +5,20 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-class FramManager:
+class FarmManager:
     def __init__(self):
-        self.db = db
-
+        self.farm_ref  = db.collection("farms")
+    
+    def create_farm(self, farm_id, data):
+        """สร้างฟาร์มใหม่"""
+        self.farm_ref.document(farm_id).set(data)
+        print(f"ฟาร์ม {farm_id}ถูกสร้างเเล้ว")
+    def add_member(self, farm_id, member_name):
+        """เพิ่มสมาชิกในฟาร์ม"""
+        farm_doc = self.farm_ref.document(farm_id)
+        farm = farm_doc.get().to_dict()
+        members =_farm.get ("members", [])
+        members.append(member_name)
+        farm_doc.update({"members": members})
+        print(f"เพิ่มสมาชิก{member_name} ในฟาร์ม {farm_id}")
         
-
