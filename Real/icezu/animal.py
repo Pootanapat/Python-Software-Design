00fed469842bcb219a8f858a4615017f0abdd68a
Tanatap Treerapragon <thanathep.ice@gmail.com>
2025-09-01 21:11:31 +0700

    อก้ไขชื่อรายางบนตาราง

--- Real/icezu/animal.py
+++ Real/icezu/animal.py
@@ -122,7 +122,7 @@ btn_summary = tk.Button(button_frame, text="สรุปข้อมูล", fon
 btn_summary.pack(side="left", padx=10)
 
 # รายงาน
-tk.Label(root, text=" รายงานการจัดเก็บ", font=font_title, bg="#f0f5f0").pack()
+tk.Label(root, text=" รายงานการจัดเก็บสัตว์", font=font_title, bg="#f0f5f0").pack()
 
 report_table = ttk.Treeview(root, columns=("ประเภทสัตว์", "จำนวน", "วันที่"), show="headings")
 report_table.heading("ประเภทสัตว์", text="ประเภทสัตว์")
